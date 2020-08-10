f1dae2fb23d582e9590f46b1efbe5fba9f76ddf2
Artem Kuznetsov <norden.spb@mail.ru>
2020-08-10 23:42:58 +0300

    Подчсет произведения элементов массива через цикл

--- Lesson02_01/script.js
+++ Lesson02_01/script.js
@@ -1,8 +1,12 @@
 let num = 266219;
 
-let arrayNumber = (num.toString().split(''));
+let arrayNumber = num.toString().split("");
 console.log(arrayNumber);
-let multy = (arrayNumber[0] * arrayNumber[1] * arrayNumber[2] * arrayNumber[3] * arrayNumber[4] * arrayNumber[5]); //сумма цифр числа
+// let multy = (arrayNumber[0] * arrayNumber[1] * arrayNumber[2] * arrayNumber[3] * arrayNumber[4] * arrayNumber[5]);
+let multy = 1;
+for (let i = 0; i < 6; i++) {
+  multy *= arrayNumber[i];
+}
 console.log(multy);
 let pow = multy ** 3; //степень
 console.log(pow);
