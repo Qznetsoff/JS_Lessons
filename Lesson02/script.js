fb1e126db8aff0b93f3f57046d9f4bf7a8c14b8e
Artem Kuznetsov <norden.spb@mail.ru>
2020-08-11 00:04:27 +0300

    Исправил на нижний регистр переменную addExpenses

--- Lesson02/script.js
+++ Lesson02/script.js
@@ -1,6 +1,6 @@
 let money = 40000;
 let income = 25000;
-let addExpenses = 'Каршеринг, кварплата, телефон, интернет';
+let addExpenses = "Каршеринг, Кварплата, Телефон, Интернет";
 let deposit = true;
 let mission = 182000;
 let period = 7;
@@ -8,9 +8,9 @@ console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length); //длина строки
-console.log('"Период равен ' + (period) + ' месяцев"');
-console.log('"Цель заработать ' + (mission) + ' рублей"');
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(', '));
+console.log('"Период равен ' + period + ' месяцев"');
+console.log('"Цель заработать ' + mission + ' рублей"');
+addExpenses = addExpenses.toLowerCase();
+console.log(addExpenses.split(", "));
 let budgetDay = money / 30;
 console.log(budgetDay);
